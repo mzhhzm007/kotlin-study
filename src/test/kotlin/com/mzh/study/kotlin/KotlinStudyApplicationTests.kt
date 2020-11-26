7d435c01fc141c7c279ad156e8a5f198c11a6661
mzhhzm007 <mzhhzm007@outlook.com>
2020-11-26 11:32:53 +0800

    init from spring.io

---
+++ src/test/kotlin/com/mzh/study/kotlin/KotlinStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mzh.study.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
